b407736f03d175d79154bdb18ad61f9f1942bff7
CUJeong <akow283@gmail.com>
2023-10-04 12:51:10 +0900

    메소드 시작

---
+++ src/chanung/submit04/Submit04.java
@@ -0,0 +1,33 @@
+package chanung.submit04;
+
+import java.util.Scanner;
+
+public class Submit04 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int elevatorA = 10;
+        int elevatorB = 4;
+
+        while(true){
+            System.out.println("========== 희영빌딩 엘리베이터 ==========");
+            System.out.println("엘리베이터 A 위치: " + elevatorA);
+            System.out.println("엘리베이터 B 위치: " + elevatorB);
+            System.out.print("몇층에 계시나요? >>> ");
+
+            // 층을 입력받기
+
+            // 입력받은 층에 대한 엘리베이터 A와의 위치 차이 구하기
+            // 입력받은 층에 대한 엘리베이터 B와의 위치 차이 구하기
+
+            // A 위치차이와 B 위치차이의 크기를 비교해서
+            // 크기가 더 작은 엘리베이터를 이동시킨다.
+
+
+        }
+
+    }
+
+}
